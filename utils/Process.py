923086852dd9330b0ad922475461bb45f6bbc27c
moyartumanley <mmanley@macalester.edu>
2024-12-04 14:37:26 -0600

    Revert "Added status logging for when site is unavailiable"
    This reverts commit 47927afdac7754d32e803fa76a7f32959964530d.

--- utils/Process.py
+++ utils/Process.py
@@ -38,11 +38,10 @@ class Process:
             self.scraper = HomePageScraper.HomePageScraper(url)
             self.url_list = self.scraper.urls_to_list()
         except:
-            # If unable to scrape URLs due to homepage being down
             logging.info("Page is inaccessible.")
             logging.info("Status Code: " + str(page.status_code))
         else:   
-            # If able to retrieve a url list, log message
+            # Log message
             if (len(self.url_list) <= 0):
                 logging.critical("URL list is empty")
             else: 
