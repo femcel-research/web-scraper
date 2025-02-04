75b6143cce5407567e21fd7567be1bce6c40f61e
moyartumanley <mmanley@macalester.edu>
2025-02-04 00:44:29 -0600

    Improved logic for master thread generation

--- crystalcafe-scraper/utils/MasterVersionGenerator.py
+++ crystalcafe-scraper/utils/MasterVersionGenerator.py
@@ -7,52 +7,52 @@ import os
 
 
 class MasterVersionGenerator:
-    # TODO: Might add logging to this part to keep track of changes. Ask Wren if they would be okay with having separate logging folders.
-    def __init__(self, original, replies, thread_meta, id, folder_path):
-        # Thread contents
-        self.original = original
-        self.replies = replies
-        self.meta = thread_meta
-
+    # new version
+    def __init__(self, thread_contents_json, thread_meta, id, folder_path):
         # File directory info
         self.thread_number = id
         self.folder_path = folder_path
         self.file_name = f"master_version_{self.thread_number}.json"
         self.file_path = os.path.join(self.folder_path, self.file_name)
 
-        # Retrieves distinct post ids from thread meta
-        distinct_reply_ids = thread_meta.get("dist_post_ids")
+        # Thread content
+        self.thread_contents = thread_contents_json
+        self.original_post = self.thread_contents["original_post"]
+        self.replies = self.thread_contents["replies"]
 
-        # Adds ids to a set to ensure no duplication.
-        self.thread_post_ids = set(distinct_reply_ids)
+        # Lost post id retrieval
+        self.thread_meta = thread_meta
+        lost_ids = thread_meta["lost_post_ids"]
 
-    def add_to_set(self, thread_replies):
-        """Adds original post and replies to a set to preserve deleted posts"""
-        for reply in thread_replies:
-            self.thread_post_ids.add(reply["post_id"])
+        self.lost_ids_set = set(lost_ids)
+
+    def check_if_post_lost(self, post_id):
+        """Checks if given post was deleted"""
+        if post_id in self.lost_ids_set:
+            return True
+        return False
 
     def generate_dict(self):
         """Generates a dictionary containing all posts on a given thread"""
         current_time = datetime.today().strftime("%Y-%m-%dT%H:%M:%S")
-        thread_replies = self.replies.values()
-        self.add_to_set(thread_replies)
         all_replies = {}
 
         thread_contents = {
             "date_of_previous_scan": "",
             "date_of_latest_scan": current_time,
             "thread_number": self.thread_number,
-            "original_post": self.original,
+            "original_post": self.original_post,
         }
 
-        # Add replies with ids recorded in set in order to prevent duplication.
-        for reply in thread_replies:
-            reply_id = reply["post_id"]
-            if reply_id in self.thread_post_ids:
-                all_replies[reply_id] = reply
+        # Adds deletion marker if needed
+        for reply in self.replies.values():
+            reply_id = reply.get("post_id")
+            if self.check_if_post_lost(reply_id):
+                reply.update({"post_lost": self.check_if_post_lost(reply_id)})
+            all_replies[reply_id] = reply
 
         # Updates scan times
-        previous_scan_time = thread_contents.get("date_of_latest_scan")
+        previous_scan_time = thread_contents["date_of_latest_scan"]
         thread_contents.update({"date_of_previous_scan": previous_scan_time})
         thread_contents.update(
             {"date_of_latest_scan": datetime.today().strftime("%Y-%m-%dT%H:%M:%S")}
@@ -75,7 +75,7 @@ class MasterVersionGenerator:
         thread_contents = self.generate_dict()
 
         # Merge existing replies with new replies
-        existing_replies = existing_data.get("replies", {})
+        existing_replies = existing_data["replies"]
         thread_contents["replies"].update(existing_replies)
 
         # Write the updated data to the file
