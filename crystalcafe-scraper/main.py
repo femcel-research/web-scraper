3200cc2b6033769ee804bac2e318753e8c69fff6
moyartumanley <mmanley@macalester.edu>
2025-02-09 02:54:32 -0600

    Working randomized portion generator. Have to rename crystalcafe data dir to crystal.cafe

--- crystalcafe-scraper/main.py
+++ crystalcafe-scraper/main.py
@@ -1,5 +1,10 @@
 from utils import Process
+from utils import PortionRetriever
 
 process = Process("https://crystal.cafe/")
 process.process_current_list()
 print("Complete!")
+
+# portion = PortionRetriever(10, "crystal.cafe")
+# portion.generate_portion()
+# print("Complete!")
