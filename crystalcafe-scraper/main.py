637cf07c6fbe1a7c1680c0d7692ff0d999d783a8
Wren Kuratana <156984488+wkuratana@users.noreply.github.com>
2025-06-11 13:09:47 -0500

    Deleted crystalcafe-scraper/ directory

--- crystalcafe-scraper/main.py
+++
@@ -1,8 +0,0 @@
-from utils.parsing.Parse import Parse
-from utils import PortionRetriever
-import sys
-
-parse = Parse("https://crystal.cafe/")
-parse.process_existing_files()
-parse.process_current_list()
-print("Complete!")
